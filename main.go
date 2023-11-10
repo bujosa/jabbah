e193ddd6fa9978b5dcc8ecfad67da6fbf9333e77
bujosa <davidbujosa@gmail.com>
2023-11-09 22:50:17 -0400

    refactor: eliminate unnecesary code

--- main.go
+++ main.go
@@ -3,44 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"jabbah/config"
 	"log"
-
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/memorydb"
-	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-	// Load the SDK's configuration from environment and shared config, and
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
-
-	fmt.Println("Region: ", cfg.Region)
-
-	// Create a new MemoryDB client
-	client := memorydb.NewFromConfig(cfg)
-
-	// Get the endpoint of your Amazon MemoryDB for Redis cluster
-	output, err := client.DescribeClusters(context.Background(), &memorydb.DescribeClustersInput{})
-	if err != nil {
-		log.Fatalf("Failed to describe clusters: %v", err)
-	}
-
-	clusterEndpoint := output.Clusters[0].ClusterEndpoint
-	endpointString := *clusterEndpoint.Address
-	endpointString += ":" + fmt.Sprint(clusterEndpoint.Port)
-	fmt.Println("Endpoint: ", endpointString)
+	// Load Enviroment
+	config.LoadEnviroment()
 
 	// Create a new Redis client using the cluster endpoint
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: endpointString, // Replace this with the endpoint of your Amazon MemoryDB for Redis cluster
-		DB:   0,              // Use the default database
-	})
+	redisClient := config.GetRedisClient()
 
 	// Set a key-value pair
-	err = redisClient.Set(context.Background(), "foo", "bar", 0).Err()
+	err := redisClient.Set(context.Background(), "foo", "bar", 0).Err()
 	if err != nil {
 		log.Fatalf("failed to set key-value pair: %v", err)
 	}
